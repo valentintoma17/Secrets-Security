73270ee7b5ef96ac3fe204a5fd12d34b724e5f62
Valentin <valentin.toma17@gmail.com>
2022-03-22 21:02:33 +0200

    changed port 3000 to heroku port

--- app.js
+++ app.js
@@ -204,6 +204,11 @@ req.login(user, function(err){
   });
 });
 
-app.listen(3000, function() {
-    console.log("Server started on port 3000.")
-    });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started succesfully.")
+});
